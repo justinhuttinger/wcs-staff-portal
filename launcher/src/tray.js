2837ff627f6c21f8e0d78310cad6d989965d7630
justinhuttinger <justin@wcstrength.com>
2026-04-06 15:26:07 -0700

    feat: system tray with show/quit menu

---
+++ launcher/src/tray.js
@@ -0,0 +1,38 @@
+const { Tray, Menu, dialog, app } = require('electron')
+const path = require('path')
+
+let tray = null
+
+function createTray(mainWindow) {
+  const iconPath = path.join(__dirname, '..', 'assets', 'tray-icon.png')
+  tray = new Tray(iconPath)
+  tray.setToolTip('WCS App')
+
+  const contextMenu = Menu.buildFromTemplate([
+    {
+      label: 'Show WCS App',
+      click: () => { mainWindow.show(); mainWindow.focus() },
+    },
+    { type: 'separator' },
+    {
+      label: 'Quit',
+      click: () => {
+        const choice = dialog.showMessageBoxSync({
+          type: 'warning',
+          title: 'Quit WCS App',
+          message: 'Are you sure you want to quit?',
+          buttons: ['Cancel', 'Quit'],
+          defaultId: 0,
+        })
+        if (choice === 1) app.quit()
+      },
+    },
+  ])
+
+  tray.setContextMenu(contextMenu)
+  tray.on('click', () => { mainWindow.show(); mainWindow.focus() })
+
+  return tray
+}
+
+module.exports = { createTray }
